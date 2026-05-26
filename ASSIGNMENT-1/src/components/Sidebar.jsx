bce221bb88e4afc3f710d1b6b4fa64a1e645e49e
Karan007-official <www.karanofficial111@gmail.com>
2026-05-26 10:40:29 +0530

    Add files via upload

---
+++ ASSIGNMENT-1/src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Support</li>
+        <li>FAQ</li>
+        <li>T&C</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
